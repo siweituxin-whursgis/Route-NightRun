55e349a47039e432d600761e1f9528b56f33b8b4
blablaxiaomoxian <350921789@qq,com>
2017-07-24 14:27:38 +0800

    新增室内运动

--- app/src/main/java/com/example/huyigong/route_nightrun/Indoor_run.java
+++ app/src/main/java/com/example/huyigong/route_nightrun/Indoor_run.java
@@ -1,6 +1,8 @@
 package com.example.huyigong.route_nightrun;
 
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.symbology.PictureMarkerSymbol;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSceneSymbol;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 
@@ -51,24 +54,29 @@ public class Indoor_run extends AppCompatActivity {
                 Point gymPt14 = new Point(114.374725,30.528779, SpatialReferences.getWgs84());
                 Point gymPt15 = new Point(114.375398,30.529055, SpatialReferences.getWgs84());
 
+                BitmapDrawable pinStarBlueDrawable = (BitmapDrawable) ContextCompat.getDrawable(Indoor_run.this, R.drawable.gymicon);
+                final PictureMarkerSymbol campsiteSymbol = new PictureMarkerSymbol(pinStarBlueDrawable);
+                campsiteSymbol.setHeight(20);
+
+                campsiteSymbol.setWidth(20);
 
 
 
-                Graphic graphic1 = new Graphic(gymPt1, symbol);
-                Graphic graphic2 = new Graphic(gymPt2, symbol);
-                Graphic graphic3 = new Graphic(gymPt3, symbol);
-                Graphic graphic4 = new Graphic(gymPt4, symbol);
-                Graphic graphic5 = new Graphic(gymPt5, symbol);
-                Graphic graphic6 = new Graphic(gymPt6, symbol);
-                Graphic graphic7 = new Graphic(gymPt7, symbol);
-                Graphic graphic8 = new Graphic(gymPt8, symbol);
-                Graphic graphic9 = new Graphic(gymPt9, symbol);
-                Graphic graphic10 = new Graphic(gymPt10, symbol);
-                Graphic graphic11= new Graphic(gymPt11, symbol);
-                Graphic graphic12= new Graphic(gymPt12, symbol);
-                Graphic graphic13= new Graphic(gymPt13, symbol);
-                Graphic graphic14= new Graphic(gymPt14, symbol);
-                Graphic graphic15= new Graphic(gymPt15, symbol);
+                Graphic graphic1 = new Graphic(gymPt1, campsiteSymbol);
+                Graphic graphic2 = new Graphic(gymPt2, campsiteSymbol);
+                Graphic graphic3 = new Graphic(gymPt3, campsiteSymbol);
+                Graphic graphic4 = new Graphic(gymPt4, campsiteSymbol);
+                Graphic graphic5 = new Graphic(gymPt5, campsiteSymbol);
+                Graphic graphic6 = new Graphic(gymPt6, campsiteSymbol);
+                Graphic graphic7 = new Graphic(gymPt7, campsiteSymbol);
+                Graphic graphic8 = new Graphic(gymPt8, campsiteSymbol);
+                Graphic graphic9 = new Graphic(gymPt9, campsiteSymbol);
+                Graphic graphic10 = new Graphic(gymPt10, campsiteSymbol);
+                Graphic graphic11= new Graphic(gymPt11, campsiteSymbol);
+                Graphic graphic12= new Graphic(gymPt12, campsiteSymbol);
+                Graphic graphic13= new Graphic(gymPt13, campsiteSymbol);
+                Graphic graphic14= new Graphic(gymPt14, campsiteSymbol);
+                Graphic graphic15= new Graphic(gymPt15, campsiteSymbol);
 
                 GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
                 MapView mMapView = (MapView) findViewById(R.id.mapView);
